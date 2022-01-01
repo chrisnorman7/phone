70a72de7b6a39941c40a15b18819d5260ae2082d
Chris Norman <chris.norman2@googlemail.com>
2022-01-01 23:15:47 +0000

    Change some log levels.

--- lib/emojis.dart
+++ lib/emojis.dart
@@ -1,4 +1,4 @@
-/// Provides the [getEmojis] class.
+/// Provides the [getEmojis] function.
 import 'dart:convert';
 import 'dart:io';
 
@@ -8,29 +8,30 @@ import 'src/json/emojis.dart';
 
 /// Returns all emojis.
 EmojiList getEmojis() {
-  final rootLogger = Logger('Load Emojis');
+  final logger = Logger('Load Emojis');
   final emojiFile = File('unicode/data/all_emojis.json');
   final data = emojiFile.readAsStringSync();
-  rootLogger.info('Loaded emoji data.');
+  logger.fine('Loaded emoji data.');
   final json = jsonDecode(data) as List<dynamic>;
-  rootLogger.info('Loaded emoji JSON.');
+  logger.fine('Loaded emoji JSON.');
   final emojiNames = <String, String>{};
   final categories = <String, EmojiCategory>{};
   final subcategories = <String, EmojiSubcategory>{};
   for (final entry in json) {
     final emoji = Emoji.fromJson(entry as Map<String, dynamic>);
+    logger.finest('Found emoji ${emoji.description}.');
     emojiNames[emoji.characters] = emoji.description;
     EmojiSubcategory? subcategory = subcategories[emoji.subcategoryName];
     if (subcategory == null) {
       subcategory = EmojiSubcategory(name: emoji.subcategoryName, emojis: []);
       subcategories[subcategory.name] = subcategory;
-      rootLogger.info('Created subcategory ${subcategory.name}.');
+      logger.finest('Created subcategory ${subcategory.name}.');
     }
     EmojiCategory? category = categories[emoji.categoryName];
     if (category == null) {
       category = EmojiCategory(name: emoji.categoryName, subcategories: []);
       categories[category.name] = category;
-      rootLogger.info('Created category ${category.name}.');
+      logger.finest('Created category ${category.name}.');
     }
     category.subcategories.add(subcategory);
     subcategory.emojis.add(emoji);
