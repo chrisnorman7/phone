eb692f550e937db0dc369190f60179a19e9e5b11
Chris Norman <chris.norman2@googlemail.com>
2022-01-01 19:46:24 +0000

    Start using common test code.

--- test/speech_test.dart
+++ test/speech_test.dart
@@ -2,38 +2,7 @@ import 'package:phone/speech.dart';
 import 'package:phone/src/json/phone_options.dart';
 import 'package:test/test.dart';
 
-/// A speech utterance.
-class SpeechUtterance {
-  /// Create an instance.
-  const SpeechUtterance({required this.text, required this.interrupt});
-
-  /// The string to speak.
-  final String text;
-
-  /// Whether or not to interrupt.
-  final bool interrupt;
-}
-
-/// A speech engine that stores strings.
-class DummySpeechEngine extends SpeechEngine {
-  /// Create an instance.
-  DummySpeechEngine()
-      : utterances = [],
-        super(system: speechSystems.first);
-
-  /// The strings spoken so far.
-  final List<SpeechUtterance> utterances;
-
-  /// Store the speech string.
-  @override
-  Future<void> speak(
-      {required String text,
-      required Map<String, String> substitutions,
-      bool interrupt = true}) async {
-    final utterance = SpeechUtterance(text: text, interrupt: interrupt);
-    utterances.add(utterance);
-  }
-}
+import 'common.dart';
 
 void main() {
   group(
