780e1973215f48e1cbebdf49abe932088054b8e7
Chris Norman <chris.norman2@googlemail.com>
2022-01-02 21:22:21 +0000

    Add a json encoder constant.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,9 @@
 /// Provides constants for use in the program.
+import 'dart:convert';
 import 'dart:io';
 
 /// The directory where log files are stored.
 final logDirectory = Directory('logs');
+
+/// The json encoder to be used everywhere.
+const jsonEncoder = JsonEncoder.withIndent('  ');
