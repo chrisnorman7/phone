9e73c71d101ac9e4106d25eb0f92fedcea159709
Chris Norman <chris.norman2@googlemail.com>
2022-01-03 12:23:51 +0000

    You can now modify the text elements of contacts.

--- lib/src/pages/contacts/edit_contact_page.dart
+++ lib/src/pages/contacts/edit_contact_page.dart
@@ -1,13 +1,36 @@
 /// Provides the [EditContactPage] class.
 import '../../../constants.dart';
+import '../../../enumerations.dart';
 import '../../../main_loop.dart';
 import '../../json/contacts.dart';
 import '../../ui/input_handler.dart';
 import '../../ui/label.dart';
 import '../../ui/widget_page.dart';
 import '../../ui/widgets/widget.dart';
+import '../editor_page.dart';
 
-/// A page used for adding a contact.
+/// Edit a contact parameter.
+Future<void> Function(MainLoop mainLoop) _editContactParameter({
+  required String label,
+  required String initialText,
+  required void Function(String text) setValue,
+}) =>
+    (mainLoop) => mainLoop.pushPage(EditorPage(
+        onDone: (text) async {
+          if (text.isNotEmpty && text != initialText) {
+            setValue(text);
+            mainLoop.contactList.save();
+          }
+          await mainLoop.popPage();
+        },
+        cursorPosition: initialText.length,
+        initialText: initialText,
+        label: () => '$label: ',
+        onCancel: (mainLoop) => mainLoop.popPage(),
+        mode:
+            initialText.isEmpty ? TypingMode.upperCase : TypingMode.lowerCase));
+
+/// A page used for editing a [contact].
 class EditContactPage extends WidgetPage {
   /// Create an instance.
   EditContactPage({required this.contact})
@@ -27,12 +50,27 @@ class EditContactPage extends WidgetPage {
       ..addAll([
         Widget(
           label: () => 'First name: ${contact.firstName}',
+          onActivate: _editContactParameter(
+            initialText: contact.firstName,
+            label: 'First name',
+            setValue: (text) => contact.firstName = text,
+          ),
         ),
         Widget(
           label: () => 'Surname: ${contact.surname}',
+          onActivate: _editContactParameter(
+            initialText: contact.surname,
+            label: 'Surname',
+            setValue: (text) => contact.surname = text,
+          ),
         ),
         Widget(
           label: () => 'Title: ${contact.title}',
+          onActivate: _editContactParameter(
+            initialText: contact.title,
+            label: 'Title',
+            setValue: (text) => contact.title = text,
+          ),
         ),
         ...[
           for (final phoneNumber in contact.phoneNumbers)
@@ -62,21 +100,20 @@ class EditContactPage extends WidgetPage {
         ),
         Widget(
           label: () => 'Website: ${contact.website}',
+          onActivate: _editContactParameter(
+            initialText: contact.website,
+            label: 'Website',
+            setValue: (text) => contact.website = text,
+          ),
         ),
         Widget(
           label: () => 'Notes: ${contact.notes}',
+          onActivate: _editContactParameter(
+            initialText: contact.notes,
+            label: 'Notes',
+            setValue: (text) => contact.notes = text,
+          ),
         ),
-        Widget(
-            label: () => mainLoop.contactList.contacts.contains(contact)
-                ? 'Save Contact'
-                : 'Add Contact',
-            onActivate: (mainLoop) async {
-              if (mainLoop.contactList.contacts.contains(contact) == false) {
-                mainLoop.contactList.contacts.add(contact);
-              }
-              mainLoop.contactList.save();
-              await mainLoop.popPage();
-            })
       ]);
     super.onPush(mainLoop);
   }
